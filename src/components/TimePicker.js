8843e1b1f25bdfc2166d0bd6977c006fb3e82734
Nabeel Anjum <timedilation2012@gmail.com>
2021-05-25 19:30:16 +0500

    time picker added and some UI fixes

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -18,7 +18,10 @@ const SelectionBar = () => (
     borderColor: Color.action,
     height: 40,
     borderRadius: 4,
-  }} />
+    justifyContent: 'center'
+  }}>
+    <View style={{ marginLeft: '34%', height: 20, width: 1, backgroundColor: Color.primaryLight }} />
+  </View>
 );
 
 class TimePicker extends React.PureComponent {
@@ -33,18 +36,41 @@ class TimePicker extends React.PureComponent {
     moment().add(6, 'days').format('ddd MMM D'),
   ];
 
+  times = [
+    'between 7 pm - 8 pm',
+    'between 7 pm - 8 pm',
+    'between 7 pm - 8 pm',
+    'between 7 pm - 8 pm',
+    'between 7 pm - 8 pm',
+    'between 7 pm - 8 pm',
+    'between 7 pm - 8 pm',
+  ];
+
   render() {
     return (
       <View style={styles.container}>
         <SelectionBar />
         <ScrollView
+          style={{ width: '35%' }}
           contentContainerStyle={styles.daysScrollContainer}
           showsVerticalScrollIndicator={false}
           snapToOffsets={[60, 120, 180, 240, 300, 360]}
         >
           {this.dates.map((date) =>
-            <View style={styles.item}>
-              <Text style={styles.dayText}>{date}</Text>
+            <View style={{ ...styles.item, height: 60, alignItems: 'flex-end' }}>
+              <Text style={{ ...styles.text, fontWeight: 'bold' }}>{date}</Text>
+            </View>
+          )}
+        </ScrollView>
+        <ScrollView
+          style={{ width: '65%' }}
+          contentContainerStyle={styles.timesScrollContainer}
+          showsVerticalScrollIndicator={false}
+          snapToOffsets={[40, 80, 120, 160, 200, 240]}
+        >
+          {this.times.map((time) =>
+            <View style={{ ...styles.item, height: 40, alignItems: 'flex-start' }}>
+              <Text style={styles.text}>{time}</Text>
             </View>
           )}
         </ScrollView>
@@ -63,13 +89,15 @@ const styles = StyleSheet.create({
     paddingTop: 110,
     paddingBottom: 80,
   },
+  timesScrollContainer: {
+    paddingTop: 120,
+    paddingBottom: 90
+  },
   item: {
-    height: 60,
     justifyContent: 'center',
     paddingHorizontal: 20,
   },
-  dayText: {
-    fontWeight: 'bold',
+  text: {
     color: '#fff'
   }
 })
